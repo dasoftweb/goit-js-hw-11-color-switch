a37716eb114d4b1d1c55999d31cbed52964b82b8
Klirik <dasoftweb@gmail.com>
2021-03-01 13:19:45 +0200

    1

--- js/main.js
+++ js/main.js
@@ -1,12 +1,36 @@
 const colors = [
-    '#FFFFFF',
-    '#2196F3',
-    '#4CAF50',
-    '#FF9800',
-    '#009688',
-    '#795548',
-  ];
+  "#FFFFFF",
+  "#2196F3",
+  "#4CAF50",
+  "#FF9800",
+  "#009688",
+  "#795548",
+];
 
-  const randomIntegerFromInterval = (min, max) => {
-    return Math.floor(Math.random() * (max - min + 1) + min);
-  };
+const bodyRef = document.querySelector("body");
+const btnStartRef = document.querySelector("[data-action='start']");
+const btnStopRef = document.querySelector("[data-action='stop']");
+
+let timerChangeColor = null;
+let intervalActive = false;
+
+const randomIntegerFromInterval = (min, max) => {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
+
+btnStartRef.addEventListener("click", () => {
+  if (intervalActive) {
+    return;
+  }
+  timerChangeColor = setInterval(() => {
+    intervalActive = true;
+
+    bodyRef.style.backgroundColor =
+      colors[randomIntegerFromInterval(0, colors.length - 1)];
+  }, 1000);
+});
+
+btnStopRef.addEventListener("click", () => {
+  clearInterval(timerChangeColor);
+  intervalActive = false;
+});
